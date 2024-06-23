ddc8f1051ae590c2a9457ab6b56ded450ac8aa12
Md. Asif Imtiyaj Chowdhury <asifniloy45@gmail.com>
2024-06-23 21:58:28 +0600

    adding readme.md file with features listing and home page code

---
+++ README.md
@@ -0,0 +1,20 @@
+## PHP Web Application Project-Telemedicine & Doctor Recommendation System Using ML Algorithm.
+
+## Features:
+1: Create User Account.
+
+2: User Authentication.
+
+3: Store Patient Details
+
+4: Give prescription to the patient according to the specific Diseases. 
+
+5: Patient can give Ratings to the available Doctor.
+
+6: Patient can see Doctor Recommendation for various diseases according to the overall ratings.
+
+7: Remember Login for Multiple Entry.
+
+## Language: HTML, CSS , PHP , SQL, JSON Data
+## DataBase: MySQL DataBase
+## Tools: Visual Studio Code, Xampp(Apache)Server.
---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="homepage_style.css">
+        <title>Home</title>
+    </head>
+    <body>
+        <div class="main">
+            <div class="navbar">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+                <a href="#">Health Care<i class="fa fa-fw fa-heart"></i> & <i class="fa fa-fw fa-mobile-phone"></i>TeleMedicine System</a>
+                <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a>
+                <a href="logout.php"><i class="fa fa-fw fa-circle"></i> Logout</a>
+            </div>
+
+            <div class="box">
+                <div>
+                    <a href="problem_page.php" class="button">Get Treatment</a>
+                </div>
+                <div>
+                    <a href="see_recomendation.php" class="button">See Recomendation</a>
+                </div>
+                <div>
+                    <a href="in.php" class="button">Give Ratings</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
